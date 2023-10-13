9e5be264e5ff8098f7ad6e43e47e227d8fd4658c
FraRussotto <russottofrancesco1@gmail.com>
2023-10-13 17:26:29 +0200

    [edit] Search with archetype filter

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ import { store } from './data/store';
 import Header from './components/Header.vue';
 import CardContainer from './components/CardsContainer.vue'
 import ResultSearch from './components/ResultSearch.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
     CardContainer,
-    ResultSearch
+    ResultSearch,
+    SearchBar
   },
   data(){
     return{
@@ -19,23 +21,38 @@ export default {
   },
   methods:{
     getApi(){
-      axios.get(store.apiUrl)
+      axios.get(store.apiUrl, {
+        params:{
+          archetype: store.objectSearch
+        }
+      })
       .then(res =>{
         store.cardList = res.data.data
       })
       .catch(err => {
         console.log(err);
       })
+    },
+    archetypeApi(){
+      axios.get(store.archetypeApi)
+      .then(res => {
+        store.archetypeList = res.data
+      })
+      .catch(err =>{
+        console.log(err);
+      })
     }
   },
   mounted(){
     this.getApi()
+    this.archetypeApi()
   }
 }
 </script>
 
 <template>
   <Header />
+  <SearchBar @startSearch="getApi"/>
   <section>
     <CardContainer />
   </section>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../data/store';
+  export default {
+    name: 'SearchBar',
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <select @change="$emit('startSearch')" class="form-select" aria-label="default selec example" v-model="store.objectSearch">
+          <option v-for="(archetype, index) in store.archetypeList" :key="index">{{ archetype.archetype_name }}</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scope>
+</style>
